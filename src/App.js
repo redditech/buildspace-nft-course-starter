858a197e3502467d29ca31376eccc49a5069e45d
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-02 14:58:36 +1000

    adds connect wallet logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,79 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const BUILDSPACE_TWITTER_HANDLE = '_buildspace';
+const BUILDSPACE_TWITTER_LINK = `https://twitter.com/${BUILDSPACE_TWITTER_HANDLE}`;
+const TWITTER_HANDLE = 'redditech';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+
+  //state variable to store user's public wallet
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    // First make sure we have access to window.ethereum
+
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+
+    // Check we're authorised to access the user's wallet
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    // Use can have multiple authorised accounts, grab the first one if it is there
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  // implement connect wallet method
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+      }
+
+      // request access to account.
+      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+
+      // This should print out the public address once we authorise Metamask
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button className="cta-button connect-wallet-button" onClick={connectWallet}>
       Connect to Wallet
     </button>
   );
+
+  // Run our function when the page loads.
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   return (
     <div className="App">
@@ -24,7 +83,11 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount===""? renderNotConnectedContainer() : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -33,7 +96,16 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
+          <span className="footer-text"> &nbsp; for my project on &nbsp;</span>
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={BUILDSPACE_TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`@${BUILDSPACE_TWITTER_HANDLE}`}</a>
+          <span className='footer-text'>&nbsp;:&nbsp;"Mint your own NFT collection and ship a Web3 app to show them off"</span>
         </div>
       </div>
     </div>
